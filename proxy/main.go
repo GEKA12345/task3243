ad4982389ba8a1b80119e7481ec91272f833e3c8
Kostya <geka4uka@gmail.com>
2025-01-10 21:01:51 +0300

    fix

---
+++ proxy/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+
+	"github.com/go-chi/chi"
+)
+
+type Router struct {
+	r *chi.Mux
+}
+
+func main() {
+	host := "http://hugo"
+	port := ":1313"
+	r := getProxyRouter(host, port)
+	http.ListenAndServe(":8080", r.r)
+}
+
+func getProxyRouter(host, port string) *Router {
+	r := &Router{r: chi.NewRouter()}
+
+	r.r.Use(NewReverseProxy(host, port).ReverseProxy)
+
+	r.r.HandleFunc("/api/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello from API"))
+	})
+	return r
+}
+
+type ReverseProxy struct {
+	host string
+	port string
+}
+
+func NewReverseProxy(host, port string) *ReverseProxy {
+	return &ReverseProxy{
+		host: host,
+		port: port,
+	}
+}
+
+// Если ресурс имеет префикс /api/, то запрос должен выдавать текст «Hello from API». Все остальные запросы должны перенаправляться на http://hugo:1313 (сервер hugo).
+func (rp *ReverseProxy) ReverseProxy(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/api/" {
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		targetURL, _ := url.Parse(rp.host + rp.port)
+
+		proxy := httputil.NewSingleHostReverseProxy(targetURL)
+		proxy.ServeHTTP(w, r)
+	})
+}
